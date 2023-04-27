a2f97a5e0a095dc2a0b6ea034c288508f4f803fc
Namra Ayub <nayub@leeddev.io>
2023-04-27 11:00:02 +0500

    final

---
+++ app/src/main/java/com/example/giffish/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.giffish
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(getApplicationContext(), "It only takes 10 to 15 minutes.", Toast.LENGTH_SHORT).show();
+
+
+
+
+    }
+}
